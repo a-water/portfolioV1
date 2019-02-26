b1e2f84b6e22b935bc97f6c1b91464cf11bfd105
Aaron <aaron_waterhouse@yahoo.com>
2019-02-25 19:29:03 -0800

    adding GA

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './js/Landing';
 import Projects from './js/Projects';
 import Contact from './js/Contact';
+import ReactGA from 'react-ga';
 
 class App extends Component {
+
+  initGA() {
+    ReactGA.initialize('UA-135211219-1');
+    ReactGA.pageview('/');
+  }
+
   render() {
+    this.initGA();
+
     return (
       <BrowserRouter>
         <div className="App">
